9b2617875f84ab85e5647418e1d1fa8ac8e00091
missvader <vanepalazon611@gmail.com>
2026-03-17 13:05:07 +0100

    feat: added styles to new component

--- components/ds-switch.js
+++ components/ds-switch.js
@@ -4,11 +4,29 @@ export class DsSwitch extends LitElement {
   static styles = [
     css`
       :host {
-        display: block;
-        color: red;
+        display: inline-block;
       }
-      :host([checked]) {
-        color: green;
+      section {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        width: 64px;
+        height: 20px;
+        border-radius: 10px;
+        background-color: #ccc;
+      }
+      span {
+        width: 32px;
+        height: 32px;
+        left: 0px;
+        position: relative;
+        border-radius: 50%;
+        background-color: #f66;
+        transition: all 0.2s linear;
+      }
+      :host([checked]) span {
+        left: 32px;
+        background-color: #6f6;
       }
     `,
   ];
@@ -22,7 +40,15 @@ export class DsSwitch extends LitElement {
   }
 
   render() {
-    return html` ${this.checked ? html`<b>ON </b>` : html`<b>OFF</b>`} `;
+    return html`
+      <section @click=${this.toggle}>
+        <span></span>
+      </section>
+    `;
+  }
+
+  toggle() {
+    this.checked = !this.checked;
   }
 }
 customElements.define("ds-switch", DsSwitch);
